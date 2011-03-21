6bb83162b3fa729e41889c6e75dc10623f82f021
Marco Rojo <mrmarcondes@gmail.com>
2011-03-21 09:47:07 -0300

    versao inicial do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PesquisaWeb::Application.config.secret_token = '7670dedefe11b88c270c6f870a0cba40bcfd58699d1a2484b67043fac5af26881379577f8356785e38a1151a1e3a6882cbadc6a57baadacdd5325b8a845771bf'
