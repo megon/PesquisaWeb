1102c5e6d91097cda12e759c281254af7fbf1881
Marco Rojo <mrmarcondes@gmail.com>
2011-04-02 14:14:51 -0300

    atualizacao da feature cadastrar participante

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110402170856) do
+ActiveRecord::Schema.define(:version => 20110402171205) do
 
   create_table "participantes", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "nome"
+    t.string   "email"
   end
 
 end
