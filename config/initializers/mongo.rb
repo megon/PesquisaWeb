1c1ee7a0b438dc6797361f691b6a52d4c9a62d13
Marco Rojo <mrmarcondes@gmail.com>
2011-05-03 14:36:33 -0300

    adicionado mongodb para PRD

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,9 +1,7 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "pesquisaWebDB-#{Rails.env}"
-
-if defined?(PhusionPassenger)
-   PhusionPassenger.on_event(:starting_worker_process) do |forked|
-     MongoMapper.connection.connect if forked
-   end
+if ENV['MONGOHQ_URL']
+  MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
+else
+  MongoMapper.config = {RAILS_ENV => {'uri' => "mongodb://localhost/pesquisaWebDB-#{Rails.env}"}}
 end
 
+MongoMapper.connect(RAILS_ENV)
