cdf73c29e9872ff02d66aa8a17159304d1d998e3
Marco Rojo <mrmarcondes@gmail.com>
2011-05-14 16:09:06 -0300

    spec da opcao de voltar

--- spec/controllers/passos_controller_spec.rb
+++ spec/controllers/passos_controller_spec.rb
@@ -15,5 +15,18 @@ describe PassosController do
       post :create, :passo => {:endereco => "endereco", :numero => "111", "passo_id" => 1}, :id => 1
       response.should redirect_to :action =>"show", :id => @resposta.passo_id.to_int + 1
     end
+
+    it "deve ir para a pagina principal se participante nao existir" do
+      
+
+    end
+
+    it "deve poder voltar para o passo anterior" do
+      @participante = mock_model(Participante).as_null_object
+      Participante.should_receive(:find_by_cpf).with(session[:cpf]).and_return(@participante)
+      @participante.respostas.should_receive(:find_by_passo_id).with("1".to_i).and_return(@resposta)
+      get :edit, :id_passo_anterior => 1
+      response.should render_template "passos/1"
+    end
   end  
 end
