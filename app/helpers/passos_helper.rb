6170507ed5d1614b83d9de19be2a2f3f378fbda4
Marco Rojo <mrmarcondes@gmail.com>
2011-07-10 15:46:17 -0300

    Permitir mostrar multiplos filhos
    Implementacao conforme ticket: https://www.pivotaltracker.com/story/show/14985479

--- app/helpers/passos_helper.rb
+++ app/helpers/passos_helper.rb
@@ -1,4 +1,7 @@
 module PassosHelper
 
-
+# exemplo de helper exibindo partial
+#  def exibir_filho (quantos, form)   
+#    render :partial => 'filhos', :locals => {:f => form, :numero => quantos}
+#  end
 end
