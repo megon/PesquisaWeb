31abd5601a49d7ccacf1020a1e1d43fa02d9ff54
Marco Rojo <mrmarcondes@gmail.com>
2011-08-01 21:40:23 -0300

    adicionado helper para link de adicionar mais indicados

--- app/helpers/indicacoes_helper.rb
+++ app/helpers/indicacoes_helper.rb
@@ -1,2 +1,8 @@
 module IndicacoesHelper
+  
+  def add_indicacao_link
+    link_to_function "Convidar mais amigos" do |page|
+      page.insert_html :bottom, :convidado, :partial => 'indicacao', :object => Indicacao.new
+    end
+  end
 end
