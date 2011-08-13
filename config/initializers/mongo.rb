e6ed55edd0fd8fbdd4149c8174e36046e4808f2e
Marco Rojo <mrmarcondes@gmail.com>
2011-08-13 16:18:39 -0300

    alteracao do banco para mongolabs

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,4 +1,4 @@
-if ENV['MONGOHQ_URL']
+if ENV['MONGOLAB_URI']
   MongoMapper.config = {Rails.env => {'uri' => ENV['MONGOLAB_URI']}}
 else
   MongoMapper.config = {Rails.env => {'uri' => "mongodb://localhost/pesquisaWebDB-#{Rails.env}"}}
