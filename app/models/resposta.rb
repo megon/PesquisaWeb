466179b1c1ac0f225fbb49670eaad41b1687eac7
Marco Rojo <mrmarcondes@gmail.com>
2011-08-13 21:37:29 -0300

    teste de retirada de atributos

--- app/models/resposta.rb
+++ app/models/resposta.rb
@@ -1,8 +1,18 @@
 class Resposta
   include MongoMapper::Document
+  
+  before_validation :clear_empty_attributes
 
   key :passo_id, Integer
 
   belongs_to :participante
+  
+  protected
+   def clear_empty_attributes
+     attributes.each do |key,value|
+       attributes.delete(key) if value.blank?
+     end
+   end  
+  
 
 end
